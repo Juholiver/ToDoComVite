320cc381d487b349aaa2c7656857a99bc19162ee
Juholiver <juholiver3@gmail.com>
2025-09-15 17:15:34 -0300

    Gerenciando o estado do formulario

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useState } from 'react';
 
 const TodoForm = () => {
+    const [value, setValue] = useState('');
+    const [category, setCategory] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value || !category) {
+            alert('Preencha todos os campos!');
+            return;
+        }
+        setValue('');
+        setCategory('');
+    };
   return (
     <div className='todo-form'>
         <h2>Adicionar Tarefa</h2>
-        <form>
-            <input type="text" placeholder="Digite a titulo" />
-            <select>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Digite a titulo" value={value} onChange={(e) => setValue(e.target.value)}/>
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
                 <option value="">Selecione uma categoria</option>
                 <option value="Trabalho">Trabalho</option>
                 <option value="Pessoal">Pessoal</option>
