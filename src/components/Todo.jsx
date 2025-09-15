c7ed55bd240e5dbba3c103b5b8d5fca20ec14f39
Juholiver <juholiver3@gmail.com>
2025-09-15 17:44:16 -0300

    Criando a Funcao de completar e deletar

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 
-const Todo = ({todo}) => {
+const Todo = ({todo, removeTodo, completeTodo}) => {
   return (
-        <div className="todo">
+        <div className="todo" style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}>
             <div className="content">
                 <p>{todo.text}</p>
                 <p className="category">({todo.category})</p> 
             </div>
             <div> 
-                <button className='complete'>Adicionar</button>
-                <button className='delete'>X</button>
+                <button className='complete' onClick = {() => completeTodo(todo.id)}>Adicionar</button>
+                < button className='delete' onClick={() => removeTodo(todo.id)}>X</button>
             </div>
     
         </div>
